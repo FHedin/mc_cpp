613c9ee960bc55c9092e2b0c31181cd872c1b95e
FHedin <hedin.florent@gmail.com>
2013-07-09 20:26:15 +0200

    A basic CMakeLists.txt was written, enough for building the current version of the program in Debug or Release modes. README need to be updated with how-to instructions concerning cmake

---
+++ include/Global_include.h
@@ -0,0 +1,26 @@
+/*  
+ *  mc_cpp : A Molecular Monte Carlo simulations software.
+ *  Copyright (C) 2013  Florent Hedin
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef	GLOBAL_INCLUDE_H
+#define	GLOBAL_INCLUDE_H
+
+#define	MC_CPP_VERSION_MAJOR	@${TGT}_VERSION_MAJOR@
+#define	MC_CPP_VERSION_MINOR	@${TGT}_VERSION_MINOR@
+
+#endif	/* GLOBAL_INCLUDE_H */
+
